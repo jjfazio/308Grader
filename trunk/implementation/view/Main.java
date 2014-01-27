f2d0f727d5be688cc3f8f4994852a9d64e36c4bf
James Fazio <james@thefazios.com>
2014-01-27 11:37:44 -0800

    added javadocs to main

--- trunk/implementation/view/Main.java
+++ trunk/implementation/view/Main.java
@@ -1,29 +1,42 @@
 package view;
 
-import java.io.IOException;
-
-import model.gradebook.Gradebook;
-import model.spreadsheet.CourseInfo;
-import model.spreadsheet.SpreadsheetCourse;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import model.gradebook.Gradebook;
+import model.spreadsheet.CourseInfo;
+import model.spreadsheet.SpreadsheetCourse;
 
 /**
- * Starting point for the application. Constructs a gradebook, adds
- * a few test courses and loads the primary stage
+ * Starting point for the application. For now constructs a gradebook
+ * and adds some test data for displaying purposes.
  * @author jamesfazio
  *
  */
 public class Main extends Application
 {
+    /**
+     * The main stage of the application.
+     */
     private Stage primaryStage;
+    
+    /**
+     * The root layout of the application.
+     */
     private BorderPane rootLayout;
+    
+    /**
+     * A reference to the current gradebook.
+     */
     private Gradebook gradebook;
 
+    /**
+     * Sets up a Gradebook with two courses, sets the current course
+     * and displays the main page.
+     */
     @Override
 	public void start(Stage primaryStage) {
 		this.primaryStage = primaryStage;
@@ -63,6 +76,10 @@ public class Main extends Application
        rootLayout.setCenter(tabs);
     }
 
+    /**
+     * Entry point of the application.
+     * @param args
+     */
     public static void main(String[] args)
     {
         launch(args);
