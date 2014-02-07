c4a3e78eaa0fefaea753a687112f25b1cb314897
Kevin Backers <kbackers@calpoly.edu>
2014-02-07 15:17:08 -0800

    more work on add grading scheme

--- trunk/implementation/model/spreadsheet/GradingScheme.java
+++ trunk/implementation/model/spreadsheet/GradingScheme.java
@@ -2,8 +2,9 @@ package model.spreadsheet;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.Observable;
 
-public class GradingScheme implements Serializable {
+public class GradingScheme extends Observable implements Serializable {
     private static final long serialVersionUID = 5306127170696546118L;
     
     Boolean plusMinusEnabled;
@@ -30,5 +31,12 @@ public class GradingScheme implements Serializable {
         plusMinusEnabled = true;
         schemeName = name;
         System.out.println("Creted a new grading scheme with name: " + name);
+    }
+    
+    public void addGradeRange(GradeRange g)
+    {
+        gradeRanges.add(g);
+        setChanged();
+        notifyObservers();
     }
 }
