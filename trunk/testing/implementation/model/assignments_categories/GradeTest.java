c42c394efa1a8f88cb1bab86103f3fa3df267156
Erik Owen <erikjowen@yahoo.com>
2014-02-26 13:56:40 -0800

    started working on testing the Grade class

--- trunk/testing/implementation/model/assignments_categories/GradeTest.java
+++ trunk/testing/implementation/model/assignments_categories/GradeTest.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package implementation.model.assignments_categories;
 
 import static org.junit.Assert.*;
@@ -9,34 +6,37 @@ import org.junit.Before;
 import org.junit.Test;
 
 /**
- * @author jamesfazio
+ * Tests the Grade class' functionality
+ * 
+ * @author erikowen
  *
  */
 public class GradeTest
 {
-
-    /**
-     * @throws java.lang.Exception
-     */
-    @Before
-    public void setUp() throws Exception
-    {
-    }
+	
+/****
+ *
+ * Class GraphTest is the companion testing class for class Graph.java.
+ * It implements the following module test plan:
+ *                                                                         <ul>
+ *                                                                      <p><li>
+ *     Phase 1: Unit test the constructor.
+ *                                                                      <p><li>
+ *     Phase 2: Unit test the setter method for TurnedIn
+ *                                                                      <p><li>
+ *     Phase 3: Unit test the getter method for TurnedIn
+ *                                                                      <p><li>
+ *     Phase 4: Unit test the setter method for the score
+ *                                                                      <p><li>
+ *     Phase 5: Unit test the getter method for the score
+ *                                                                      <p><li>
+ */
 
     /**
      * Test method for {@link model.assignments_categories.Grade#Grade(model.assignments_categories.Assignment, java.util.Date, java.lang.Double, java.lang.String)}.
      */
     @Test
     public void testGrade()
-    {
-        fail("Not yet implemented");
-    }
-
-    /**
-     * Test method for {@link model.assignments_categories.Grade#getAssign()}.
-     */
-    @Test
-    public void testGetAssign()
     {
         fail("Not yet implemented");
     }
@@ -64,6 +64,15 @@ public class GradeTest
      */
     @Test
     public void testGetScore()
+    {
+        fail("Not yet implemented");
+    }
+    
+    /**
+     * Test method for {@link model.assignments_categories.Grade#getScore()}.
+     */
+    @Test
+    public void testSetScore()
     {
         fail("Not yet implemented");
     }
