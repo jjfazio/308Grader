4a15e56c86f82d058a6ab2b6527a4b7e80d28f17
Erik Owen <erikjowen@yahoo.com>
2014-02-26 16:00:28 -0800

    updated GradeTest.java

--- trunk/testing/implementation/model/assignments_categories/GradeTest.java
+++ trunk/testing/implementation/model/assignments_categories/GradeTest.java
@@ -2,6 +2,11 @@ package implementation.model.assignments_categories;
 
 import static org.junit.Assert.*;
 
+import java.util.Date;
+
+import model.assignments_categories.Grade;
+import model.exception.BadDataException;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -32,49 +37,155 @@ public class GradeTest
  *                                                                      <p><li>
  */
 
+	
     /**
-     * Test method for {@link model.assignments_categories.Grade#Grade(model.assignments_categories.Assignment, java.util.Date, java.lang.Double, java.lang.String)}.
+     * Unit test the Grade constructor by building a Grade object.
+     *                                                              
+     *  Test
+     *  Case    Input                    Output             Remarks
+     * ====================================================================
+     *   1      grade != null            null               Grade should be properly initialized
+     *   2      grade == null			 null				Improper data field passed in, BadDataException thrown
+     *
      */
     @Test
     public void testGrade()
     {
-        fail("Not yet implemented");
+        try {
+			Grade grade = new Grade(new Date(), "95.2");
+			assertTrue(grade != null);
+		}
+        catch (BadDataException e) {
+        	/*Should never throw a bad data exception with the string "95.2"*/
+			fail();
+		}
+        
+        Grade grade2 = null;
+        try {
+        	grade2 = new Grade(new Date(), ".8.92");
+        }
+        catch (BadDataException e) {
+        	assertTrue(grade2 == null);
+		}
     }
 
     /**
-     * Test method for {@link model.assignments_categories.Grade#getTurnedIn()}.
+     * Unit test the Graph's getTurnedIn() method.
+     *                                                              
+     *  Test
+     *  Case    Input                          Output             Remarks
+     * ====================================================================
+     *   1      grade != null                  null               Grade should be properly initialized
+     *   2      grade == null			       null				  Improper data field passed in, BadDataException thrown
+     *   3      grade.getTurnedIn throws NPE   null               Accessing getTurnedIn throws NPE if constructor fails
+     *
      */
     @Test
     public void testGetTurnedIn()
     {
-        fail("Not yet implemented");
+    	Date curDate = new Date();
+    	Grade grade = null;
+        try {
+			grade = new Grade(curDate, "96.2");
+		}
+        catch (BadDataException e) {
+        	/*Data is properly formatted, should not reach this block*/
+			fail();
+		}
+        
+        assertEquals(curDate, grade.getTurnedIn());
+        
+        Date curDate2 = new Date();
+        Grade grade2 = null;
+        boolean grade2IsNull = false;
+        try {
+        	grade2 = new Grade(curDate2, "-100");
+        }
+        catch(BadDataException e) {
+        	assertTrue(grade2 == null);
+        	try {
+        		grade2.getTurnedIn();
+        	}
+        	catch(NullPointerException ex) {
+        		grade2IsNull = true;
+        	}
+        }
+        assertTrue(grade2IsNull);
     }
 
     /**
-     * Test method for {@link model.assignments_categories.Grade#setTurnedIn(java.util.Date)}.
+     * Unit test the Grade's setTurnedIn() method.
+     *                                                              
+     *  Test
+     *  Case    Input                                        Output             Remarks
+     * =========================================================================================
+     *   1      fail() when grade.setTurnedIn() gets called  null               Grade should not be able to setTurnedIn() if Grade object was not properly initialized
+     *   2      grade == null			                     null				Improper data field passed in, BadDataException thrown
+     *   3      grade2.getTurnedIn() == newDate              null               the setTurnedIn() method works if getTurnedIn() method returns new date that was set
+     *
      */
     @Test
     public void testSetTurnedIn()
     {
-        fail("Not yet implemented");
+    	Grade grade = null;
+        try {
+        	grade = new Grade(new Date(), "A+");
+        	/*The code below should never get called because above line throws BDE*/
+        	grade.setTurnedIn(new Date());
+        	fail();
+        }
+        catch(BadDataException e) {
+        	assertTrue(grade == null);
+        }
+        
+        try {
+        	Date newDate = new Date(2014, 4, 4);
+			Grade grade2 = new Grade(new Date(), "44.1");
+			grade2.setTurnedIn(newDate);
+			
+			assertEquals(newDate, grade2.getTurnedIn());
+		}
+        catch (BadDataException e) {
+			fail();
+		}
     }
 
     /**
-     * Test method for {@link model.assignments_categories.Grade#getScore()}.
+     * Unit test the Grade's getScore() method.
+     *                                                              
+     *  Test
+     *  Case    Input                                 Output             Remarks
+     * =========================================================================================
+     *   1      93.2 == grade.getScore()              null               Grade should not be able to setTurnedIn() if Grade object was not properly initialized
+     *   
      */
     @Test
     public void testGetScore()
     {
-        fail("Not yet implemented");
+        try
+        {
+			Grade grade = new Grade(new Date(), "93.2");
+			assertEquals(new Double(93.2), grade.getScore());
+			
+		}
+        catch (BadDataException e) {
+			fail();
+		}
     }
     
     /**
-     * Test method for {@link model.assignments_categories.Grade#getScore()}.
+     * Unit test the Grade's setScore() method.
+     *                                                              
+     *  Test
+     *  Case    Input                                 Output             Remarks
+     * =========================================================================================
+     *   1      ------------------------              null               Grade should not be able to setTurnedIn() if Grade object was not properly initialized
+     *   
      */
     @Test
     public void testSetScore()
     {
-        fail("Not yet implemented");
+        /*fail("Not yet implemented");*/
     }
 
     /**
@@ -83,7 +194,7 @@ public class GradeTest
     @Test
     public void testGetLetterGrade()
     {
-        fail("Not yet implemented");
+        /*fail("Not yet implemented");*/
     }
 
 }
