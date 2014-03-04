78bfc196a35338c3088a14ae664a8962a01b9087
Erik Owen <erikjowen@yahoo.com>
2014-03-04 00:06:35 -0800

    fixed issue causing NPE on 1% granularity option when viewing graphs

--- trunk/implementation/model/graph/Graph.java
+++ trunk/implementation/model/graph/Graph.java
@@ -219,7 +219,7 @@ public class Graph implements Serializable {
 		for(Student stud : this.studentList) {
 			HashMap<Integer, Grade> studGrades = stud.getGrades();
 			if(studGrades.containsKey(this.ass.getID())) {
-				Double studScore = studGrades.get(this.ass).getScore() / this.ass.getMaxPoints().doubleValue() * HUNDRED_PERCENT;
+				Double studScore = studGrades.get(this.ass.getID()).getScore() / this.ass.getMaxPoints().doubleValue() * HUNDRED_PERCENT;
 				Double studScoreFloor = Math.floor(studScore);
 				Integer studScoreFloorInt = studScoreFloor.intValue();
 				Integer numCurrentScore = returnMap.get(studScoreFloorInt.toString());
