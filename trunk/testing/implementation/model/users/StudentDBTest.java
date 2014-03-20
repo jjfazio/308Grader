381b2379d1b8ceb6bf6edc26e4c603b50ee4ff44
kfeutz <kevinfeutz@gmail.com>
2014-03-20 12:47:22 -0700

    Fixed testing errors and updated work-breakdown

--- trunk/testing/implementation/model/users/StudentDBTest.java
+++ trunk/testing/implementation/model/users/StudentDBTest.java
@@ -71,7 +71,7 @@ public class StudentDBTest
             "spring","01","308","Computer Science", 2014);
         StudentDB database = StudentDB.getInstance();
         List<Student> students = database.getStudentsForClass(courseInfo);
-        assertEquals("aaccf", students.get(0).getFirstName());
+        assertEquals("aaron", students.get(0).getFirstName());
     }
 
 }
